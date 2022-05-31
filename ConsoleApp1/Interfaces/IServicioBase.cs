3fd32e821af649463324727a9195949a9fa49f0c
ArleysGatica <gatica.arleys16@gmail.com>
2022-05-31 14:53:53 -0600

    pzw11

--- ConsoleApp1/Interfaces/IServicioBase.cs
+++ ConsoleApp1/Interfaces/IServicioBase.cs
@@ -7,7 +7,6 @@ using App.Dominio.Interfaces.Repositorio;
 
 namespace App.Aplicaciones.Interfaces
 {
-
 	public interface IServicioBase<TEntidad, TEntidadID>
 		: IAgregar<TEntidad>, IEditar<TEntidad>, IEliminar<TEntidadID>, IListar<TEntidad, TEntidadID>
 	{ }
